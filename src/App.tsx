f5d68a14bd30ea2ef661a04ef7ac3e3bf79f789c
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-13 13:22:01 -0500

    Changed context to use the interface method.

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,21 @@ import { LoginForm } from './components/login_form/LoginForm';
 import { createContext, useState } from 'react';
 import { Home } from './components/home/Home';
 
-export interface LoggedInProp {
+interface LoggedInProp {
   loggedIn: boolean,
   setLogIn: (loggedIn: boolean) => void;
 }
 
-export const LoggedInContext = createContext<{
-  loggedIn: boolean;
-  setLogIn: React.Dispatch<React.SetStateAction<boolean>>;
-}>({
+export const LogInContext = createContext<LoggedInProp>({
   loggedIn: false,
-  setLogIn: () => { }
-});
-
-// interface LogginInProp {
-//   loggedIn: boolean,
-//   setLogIn: React.SetStateAction<boolean>;
-// }
-
-// export const LogInContext = createContext<LoggedInProp>({
-//   loggedIn: false,
-//   setLogIn: () => {}
-// })
+  setLogIn: () => {}
+})
 
 function App() {
   const [loggedIn, setLogIn] = useState<boolean>(false);
 
   return (
-    <LoggedInContext.Provider value={{ loggedIn, setLogIn }}>
+    <LogInContext.Provider value={{ loggedIn, setLogIn }}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
