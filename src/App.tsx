0165a1eea2d7acb6c8fad7af78939ba3cfed4337
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-14 21:05:50 -0500

    Changed root URL to reroute to login URL and removed unncessary Home component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import { LoginForm } from './components/login_form/LoginForm';
-import { Home } from './components/home/Home';
 import { RegistrationForm } from './components/registration_form/RegistrationForm';
 import { NavBar } from './components/nav_bar/NavBar';
 import { LOGIN_URL, MENU_URL, REGISTRATION_URL, ROOT_URL } from './consts/PageUrls';
@@ -10,7 +9,7 @@ function App() {
   return (<>
     <BrowserRouter>
       <Routes>
-        <Route path={ROOT_URL} element={<LoginForm />}></Route>
+        <Route path={ROOT_URL} element={<Navigate to={LOGIN_URL} />}></Route>
         <Route path={REGISTRATION_URL} element={<RegistrationForm />}></Route>
         <Route path={LOGIN_URL} element={<LoginForm />}></Route>
         <Route path={MENU_URL} element={<NavBar />}></Route>
